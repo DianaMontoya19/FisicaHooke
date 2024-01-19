8fe1b753d7590c9c969ab1f5775d1b930a671965
Diana Carolina <dicaro2001@hotmail.com>
2024-01-19 17:55:10 -0500

    final

--- Assets/Script/Nav.cs
+++ Assets/Script/Nav.cs
@@ -6,16 +6,34 @@ using UnityEngine.AI;
 
 public class Nav : MonoBehaviour
 {
-    public Transform Objetivo;
-    void Start()
+    //public Transform Objetivo;
+    //void Start()
+    //{
+    //    NavMeshAgent agent = GetComponent<NavMeshAgent>();
+    //    agent.destination = Objetivo.position;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private NavMeshAgent agent;
+    private void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = Objetivo.position;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+             RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                agent.destination=hit.point;
+            }
+        }
     }
 }
